cf5a95a3a8faaa9731c83be581a9b1cd6462df19
Aditya Jaiswar <adityajaiswar11@gmail.com>
2025-01-04 21:02:54 +0530

    install nodemon and fix login api

--- controllers/auth.js
+++ controllers/auth.js
@@ -34,42 +34,35 @@ const signup = async (req, res) => {
   }
 };
 
-const login = (req, res) => {
-  const { email, password } = req.body;
-  console.log(email,password)
-  User.findOne({ email })
-    .then(async(user) => {
-      // console.log(user.password)
-      if (!user) return res.status(400).send("Invalid Email or Password");
-      
-      const validPass =await comparePassword(password, user.password);
-      // console.log("check true or false",validPass)
-      if (!validPass) return res.status(400).send("Invalid Password");
-      user.password = undefined;
-      if (validPass) return res.status(200).send(user);
-      // //create and assign a token
-      // const token = jwt.sign({ _id: user._id }, process.env.SECRET);
-      // //return jsonwebtoken in the response header
-      // return res.header("auth-token", token).send(token);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find the user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Email or Password" });
+    }
+
+    // Verify the password
+    const validPass = await comparePassword(password, user.password);
+    if (!validPass) {
+      return res.status(400).json({ message: "Invalid Email or Password" });
+    }
+
+    // Remove password from the user object before sending
+    const { password: _, ...userWithoutPassword } = user.toObject();
+
+    // Return the user details
+    return res.status(200).json(userWithoutPassword);
+
+  } catch (error) {
+    console.error("Login Error:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
-const userId = async (req, res) => {
-  // const user = req.params.userId;
-  // const userName = await User.findOne({ _id: user });
-  // try {
-  //   return res.json(userName);
-  // } catch (e) {
-  //   console.log(e);
-  //   return res.status(404).send("user not found");
-  // }
-  res.send("Server is running")
-};
 module.exports = {
   signup,
   login,
-  userId,
 };
