25617a00dff45b3a80a9117ac8f26e34acfd06cb
aditya <adityajaiswar11@gmail.com>
2026-01-08 00:11:25 +0530

    fix: fixed server connection

--- models/user.js
+++ models/user.js
@@ -3,24 +3,24 @@ let Schema = mongoose.Schema;
 
 // Define the User schema object
 let userSchema = new Schema({
-    name:{
-          type: String,
-          trim:true,
-          required:true
-    },
-    email:{
-          type: String,
-          trim:true,
-          required:true,
-          uniquie:true
-    },
-    password:{
-          type: String,
-          required:true
-    },
-    profile:String,
+	name: {
+		type: String,
+		trim: true,
+		required: true
+	},
+	email: {
+		type: String,
+		trim: true,
+		required: true,
+		uniquie: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	profile: String,
 });
 
 const User = mongoose.model("User", userSchema);
 
-module.exports=User
+module.exports = User
