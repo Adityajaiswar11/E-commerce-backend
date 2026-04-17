07f42922ddf785421d9acd171159c1b63a82ad13
aditya <adityajaiswar11@gmail.com>
2026-04-17 23:54:24 +0530

    feat: implement authentication routes for signup, login, and user retrieval

--- routes/auth.js
+++ routes/auth.js
@@ -6,12 +6,9 @@ const { requireSignin } = require('../middleware/auth');
 
 const  router = express.Router();
 
-router.post("/signup",signup);
-router.post("/login",login);
-
-// We add 'requireSignin' in the middle. 
-// Now, this route expects a 'Bearer token...' in the Authorization Header!
-router.get("/user", requireSignin, allUserList);
+router.post("/auth/signup", signup);
+router.post("/auth/login", login);
+router.get("/me", requireSignin, allUserList);
 
 
 module.exports=router 
