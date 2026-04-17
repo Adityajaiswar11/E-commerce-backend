0cb04e72a84f231ba6085235c3ae15eba52ebe03
aditya <adityajaiswar11@gmail.com>
2026-04-18 00:51:37 +0530

    refactor: update cors configuration to allow all origins and specify allowed methods and headers

--- index.js
+++ index.js
@@ -8,15 +8,11 @@ const app = express();
 mongoose.set("strictQuery", false);
 const bodyParser = require("body-parser");
 
-//middleware
-const allowedOrigins = [
-  "http://localhost:5173",             // local dev
-  "http://localhost:3000",             //local dev
-  "https://prodeazyshop.vercel.app",   // production frontend
-  "https://deveasyshop.vercel.app"     // staging frontend
-];
-
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.options("*", cors());
 
 
