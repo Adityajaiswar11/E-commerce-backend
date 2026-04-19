a3a0344d2036e5682bf5fb72f373249d32547639
aditya <adityajaiswar11@gmail.com>
2026-04-19 14:14:14 +0530

    refactor: remove unused legacy configuration files and associated helper utilities

--- routes/product.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const getProductList = require('../controllers/product');
-
-const router = express.Router();
-
-router.get("/product", getProductList.getProductList);
-router.get("/product/:id", getProductList.getProductById);
-
-module.exports = router 
