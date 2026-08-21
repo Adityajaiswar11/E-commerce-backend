64f2d136b9b9d3c461935b3ec5caa06ac1f326c5
aditya <adityajaiswar11@gmail.com>
2026-08-22 02:30:09 +0530

    fix

--- modules/payment/payment.routes.js
+++ modules/payment/payment.routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const paymentController = require('./payment.controller');
 const { isAuthenticated } = require('../../middleware/auth');
+const webhook = require('../webhook/webhook');
 
 const paymentRoutes = express.Router();
 
 paymentRoutes.post("/payment/order", isAuthenticated, paymentController.createOrder);
 paymentRoutes.post("/payment/verify", isAuthenticated, paymentController.verifyPayment);
+paymentRoutes.post("/webhook", webhook);
 
 
 module.exports = paymentRoutes;  
---
+++ modules/webhook/webhook.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const crypto = require("crypto");
+
+
+ const webhook = async (req, res) => {
+    try {
+      const webhookSignature = req.headers["x-razorpay-signature"];
+
+      const expectedSignature = crypto
+        .createHmac("sha256", process.env.RAZORPAY_WEBHOOK_SECRET)
+        .update(req.body)
+        .digest("hex");
+
+      if (webhookSignature !== expectedSignature) {
+        return res.status(400).json({
+          success: false,
+          message: "Invalid webhook signature",
+        });
+      }
+
+      const event = JSON.parse(req.body.toString());
+
+      console.log("Razorpay Event:", event.event);
+
+      // Payment successful
+      if (event.event === "payment.captured") {
+        const payment = event.payload
+        await supabase.from("payments").update({ status: "success", metadata: payment }).eq("order_id", payment.order_id);
+      }
+
+      // Payment failed
+      if (event.event === "payment.failed") {
+        const payment = event.payload;
+        await supabase.from("payments").update({status: "failed", metadata: payment}).eq("order_id", payment.order_id);
+      }
+
+      return res.status(200).json({
+        success: true,
+      });
+
+    } catch (error) {
+      console.error("Razorpay webhook error:", error);
+
+      return res.status(500).json({
+        success: false,
+        message: "Webhook processing failed",
+      });
+    }
+  }
+
+  module.exports = webhook;
